fe6b30a4588131f732fb385a122161b72349398a
nasstyzay <nasstyzay@mail.ru>
2024-07-17 13:35:37 +0300

    add

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 
 DATA = {
     'omlet': {
@@ -16,15 +17,23 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe_view(request, recipe):
+    if recipe not in DATA:
+        return HttpResponseNotFound('Рецепт не найден.')
+
+    servings = request.GET.get('servings', 1)
+    try:
+        servings = int(servings)
+        if servings < 1:
+            raise ValueError
+    except ValueError:
+        return HttpResponseNotFound('Количество порций должно быть положительным целым числом.')
+
+    ingredients = DATA[recipe]
+    context = {
+        'recipe': {ingredient: amount * servings for ingredient, amount in ingredients.items()}
+    }
+    return render(request, 'calculator/recipe.html', context)
