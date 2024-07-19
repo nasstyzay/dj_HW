33ac30c00eee0268bc7f6f0a8ad2840c392b411f
nasstyzay <nasstyzay@mail.ru>
2024-07-19 13:47:22 +0300

    add

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,16 +2,31 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from work_with_database.phones.models import Phone
+from django.utils.text import slugify
+
+mkdir -p work_with_database/phones/management/commands/
+touch work_with_database/phones/management/__init__.py
+touch work_with_database/phones/management/commands/__init__.py
+
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
+    help = 'Import phones from a CSV file'
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
+        with open('phones.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                phone = Phone(
+                    id=row['id'],
+                    name=row['name'],
+                    price=row['price'],
+                    image=row['image'],
+                    release_date=row['release_date'],
+                    lte_exists=row['lte_exists'],
+                    slug=slugify(row['name'])
+                )
+                phone.save()
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        self.stdout.write(self.style.SUCCESS('Successfully imported phones'))
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
+from .models import Phone
 
+def catalog(request):
+    sort_by = request.GET.get('sort', 'name')
 
-def index(request):
-    return redirect('catalog')
+    if sort_by == 'price_asc':
+        phones = Phone.objects.all().order_by('price')
+    elif sort_by == 'price_desc':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all().order_by('name')
 
+    return render(request, 'catalog/catalog.html', {'phones': phones})
 
-def show_catalog(request):
-    template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
-
-
-def show_product(request, slug):
-    template = 'product.html'
-    context = {}
-    return render(request, template, context)
+def phone_detail(request, slug):
+    phone = get_object_or_404(Phone, slug=slug)
+    return render(request, 'catalog/phone_detail.html', {'phone': phone})
